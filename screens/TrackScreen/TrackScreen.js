62f75ce3052568f0281d6d668706fd38ffdbf4bb
Yauheni Kavalionak <ekos@ekos.instinctools.ru>
2018-07-04 12:24:22 +0300

    Added showing current position on map

--- screens/TrackScreen/TrackScreen.js
+++ screens/TrackScreen/TrackScreen.js
@@ -1,24 +1,73 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Platform, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
 import MapView, { Overlay } from 'react-native-maps';
+import { Constants, Location, Permissions } from 'expo';
 import styles from './styles';
 
 import Header from '../../components/Header';
 
 export default class TrackScreen extends Component {
+  state = {
+    location: null,
+    errorMessage: null,
+  };
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice && false) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+    this.setState({ location });
+
+    this._watchPositionAsync();
+  };
+
+  _watchPositionAsync() {
+    Location.watchPositionAsync({ 
+      enableHighAccuracy: true,
+      timeInterval: 100,
+    }, location => {
+      this.setState({ location });
+
+    });
+  }
 
   render() {
+    let coords = {
+      latitude: 0,
+      longitude: 0,
+    };
+
+    if (this.state.location) {
+      coords = this.state.location.coords;
+    }
+
     return (
       <View style={styles.containerStyle}>
         <Header />
         <MapView
           style={styles.mapStyle}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+          showsUserLocation={true}
+          region={{
+            latitude: coords.latitude,
+            longitude: coords.longitude,
+            latitudeDelta: 0.0043,
+            longitudeDelta: 0.0034
           }}
         />
 
