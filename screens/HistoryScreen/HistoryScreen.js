d3cc8848b954861d4fd8265a0c120beba6cf5f93
Yauheni Kavalionak <ekos@ekos.instinctools.ru>
2018-07-04 14:40:42 +0300

    Added Hisory screen

--- screens/HistoryScreen/HistoryScreen.js
+++ screens/HistoryScreen/HistoryScreen.js
@@ -1,17 +1,76 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { List, ListItem, Text } from 'react-native-elements'
+
 import styles from './styles';
 
 import Header from '../../components/Header';
 
 export default class HistoryScreen extends React.Component {
 
+  list = [
+    {
+      date: '07.06.2018',
+      subtitle: 'Time: 00:00:00, Distance: 3.1 km'
+    },
+    {
+      date: '06.06.2018',
+      subtitle: 'Time: 00:00:00, Distance: 3.1 km'
+    },
+    {
+      date: '05.06.2018',
+      subtitle: 'Time: 00:00:00, Distance: 3.1 km'
+    },
+    {
+      date: '04.06.2018',
+      subtitle: 'Time: 00:00:00, Distance: 3.1 km'
+    },
+    {
+      date: '03.06.2018',
+      subtitle: 'Time: 00:00:00, Distance: 3.1 km'
+    },
+    {
+      date: '02.06.2018',
+      subtitle: 'Time: 00:00:00, Distance: 3.1 km'
+    },
+    {
+      date: '01.06.2018',
+      subtitle: 'Time: 00:00:00, Distance: 3.1 km'
+    },
+    {
+      date: '30.05.2018',
+      subtitle: 'Time: 00:00:00, Distance: 3.1 km'
+    },
+    {
+      date: '09.05.2018',
+      subtitle: 'Time: 00:00:00, Distance: 3.1 km'
+    },
+  ]
+
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.containerStyle}>
         <Header />
 
-        <Text>History</Text>
+        <ScrollView>
+
+          <Text style={styles.titleStyle}>My History</Text>
+
+          <List containerStyle={styles.listStyle}>
+            {
+              this.list.map((l, i) => (
+                <ListItem
+                  leftIcon={{name: 'explore', style: styles.leftIconStyle}}
+                  key={i}
+                  title={l.date}
+                  subtitle={l.subtitle}
+                  titleStyle={styles.itemTitleStyle}
+                />
+              ))
+            }
+          </List>
+
+        </ScrollView>
       </View>
     );
   }
--- screens/HistoryScreen/styles.js
+++ screens/HistoryScreen/styles.js
@@ -1,10 +1,27 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
+  containerStyle: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#7D9CFF',
+    backgroundColor: '#ffffff',
+  },
+  listStyle: {
+    marginTop: 10,
+    borderTopColor: 'green',
+    borderTopWidth: 4,
+  },
+  titleStyle: {
+    textAlign: 'center',
+    width: '100%',
+    color: '#aaaaaa',
+    paddingTop: 10,
+  },
+  itemTitleStyle: {
+    color: 'green',
+  },
+  leftIconStyle: {
+    color: 'green',
   },
 });
 
