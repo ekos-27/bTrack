1406c2fca89b9323f3fb4626df1a54c04a5d0dc6
Yauheni Kavalionak <ekos@ekos.instinctools.ru>
2018-07-11 16:01:14 +0300

    Added redux-pesist

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import MainNavigator from './navigators/MainNavigator';
 
 import { Provider } from 'react-redux';
-import store from './store';
+import { store, persistor } from './store';
+
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <MainNavigator />
+        <PersistGate persistor={persistor}>
+          <MainNavigator />
+        </PersistGate>
       </Provider>
     );
   }
--- store/index.js
+++ store/index.js
@@ -1,6 +1,17 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel1 from 'redux-persist/lib/stateReconciler/autoMergeLevel1';
+
 import reducers from '../reducers';
 
-const store = createStore(reducers);
-
-export default store;
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel1
+ };
+ 
+ const pReducer = persistReducer(persistConfig, reducers);
+ 
+ export const store = createStore(pReducer);
+ export const persistor = persistStore(store);
